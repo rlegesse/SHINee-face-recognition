8cb308fab1c27d7b1e1cd8ba45f4bd7cf29e6588
Rachel <rachel.legesse@gmail.com>
2021-11-23 10:33:22 +0900

    add option to keep or delete each duplicate

--- detect_and_remove.py
+++ detect_and_remove.py
@@ -25,7 +25,7 @@ def hamming_distance(hash1, hash2):
 	b = str(hash2)
 	b = b.rjust(20,"0")
 
-	print("Hash 1: {hash_1} Hash 2: {hash_2}".format(hash_1=a, hash_2=b))
+	#print("Hash 1: {hash_1} Hash 2: {hash_2}".format(hash_1=a, hash_2=b))
 	distance = 0
 
 	# check that hashes are same length
@@ -59,31 +59,31 @@ for imagePath in imagePaths:
 	# load the input image and compute the hash
 	image = cv2.imread(imagePath)
 	h = dhash(image)
-        print("current image: ", imagePath)
+	#print("current image: ", imagePath)
 	
 	if i == 0: # if this is the first iteration
 		p = []
-                p.append(imagePath)
-                hashes[h] = p
-                print("\n first hash: {hash1}  path: {path1}".format(hash1=h, path1=p))
-                i +=1
+		p.append(imagePath)
+		hashes[h] = p
+		#print("\n first hash: {hash1}  path: {path1}".format(hash1=h, path1=p))
+		i +=1
 	elif i > 0: 
-                unique = False
+		unique = False
 		#iterate over existing hashes in dictionary
 		for hash_ in list(hashes):
 			# compare current image's hash to hash in dictionary
 			if hash_ != None:
 				distance = hamming_distance(hash_, h)
-				print("comparing {image1} with {image2}".format(image1 = imagePath, image2=hashes[hash_]))
+				#print("comparing {image1} with {image2}".format(image1 = imagePath, image2=hashes[hash_]))
 				# if images are similar, add image path to hashes' list
 				if distance <= args["hamming_distance"]:
 					print("similar!")
 					p = hashes.get(hash_, [])
 					p.append(imagePath)
 					hashes[hash_] = p
-                                        print("hash for: {name}... appending current path: {path2} \n".format(name=hashes[hash_], path2=p))
-                                        unique = False
-                                        break
+					#print("hash for: {name}... appending current path: {path2} \n".format(name=hashes[hash_], path2=p))
+					unique = False
+					break
 				else: # save current image hash as a new entry in dictionary
 					unique = True
 			else: print("No hash")
@@ -92,34 +92,55 @@ for imagePath in imagePaths:
                     p = []
                     p.append(imagePath)
                     hashes[h] = p
-                    print("new hash entry \n")
+                    #print("new hash entry \n")
                             
 			
+i = 0
 
 # loop over the image hashes
 for (h, hashedPaths) in hashes.items():
-	print(hashedPaths)
+	
+	#print(hashedPaths)
         # check to see if there is more than one image with the same hash
 	if len(hashedPaths) > 1:
 		print("there is more than one image with this hash")
-                # check to see if this is a dry run
-		if args["remove"] <= 0:
-			# initialize a montage to store all images with the same
-			# hash
-			montage = None
-			# loop over all image paths with the same hash
-			for p in hashedPaths:
-				# load the input image and resize it to a fixed width
-				# and heightG
-				image = cv2.imread(p)
-				image = cv2.resize(image, (150, 150))
-				# if our montage is None, initialize it
-				if montage is None:
-					montage = image
-				# otherwise, horizontally stack the images
+		
+		# initialize a montage to store all images with the same hash
+		montage = None
+		
+		# loop over all image paths with the same hash
+		for p in hashedPaths:
+			# load the input image and resize it to a fixed width
+			# and heightG
+			image = cv2.imread(p)
+			image = cv2.resize(image, (150, 150))
+			# if our montage is None, initialize it
+			if montage is None:
+				montage = image
+			# otherwise, horizontally stack the images
+			else:
+				montage = np.hstack([montage, image])
+				# show the montage for the hash
+				#print("[INFO] hash: {}".format(h))
+				cv2.imshow("Montage", montage)
+				
+				# check to see if this is a dry run
+				if args["remove"] <= 0:
+					cv2.waitKey(0)
+				
 				else:
-					montage = np.hstack([montage, image])
-			# show the montage for the hash
-			print("[INFO] hash: {}".format(h))
-			cv2.imshow("Montage", montage)
-			cv2.waitKey(0)
+					print("To delete the image, press 'd'. To keep, press 'k'")
+					wait = True
+					while wait:
+						if cv2.waitKey(0) == ord('d'):
+							os.rename(p,"./duplicates/" + str(i))
+							i+=1
+							print("deleted {p}".format(p=p))
+							wait = False
+						elif cv2.waitKey(0) == ord('k'):
+							wait = False
+							print("skipped")
+						else:
+							print("invalid key!")
+					
+
